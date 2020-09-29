617709123793a2dc9e616f6368dafa668a448842
Andrew Arrow <oneone@gmail.com>
2020-09-29 15:04:22 -0700

    custom 404

--- main.go
+++ main.go
@@ -15,10 +15,15 @@ func AboutUs(c *gin.Context) {
 	c.String(http.StatusOK, "we are a new website")
 }
 
+func Custom404(c *gin.Context) {
+	c.String(404, "we have no page for this url, yet.")
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/", WelcomeIndex)
-	router.GET("/about-us", AboutUs) // < - notice the new route
+	router.GET("/about-us", AboutUs)
+	router.NoRoute(Custom404) //         <------- notice the special NoRotue func
 	port := 3000
 	router.Run(fmt.Sprintf(":%d", port))
 }
