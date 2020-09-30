c8f39a19441584aa8c4bee32946f29667f1f1e69
Andrew Arrow <oneone@gmail.com>
2020-09-29 17:58:28 -0700

    006 images

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func WelcomeIndex(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{}) // 1. load html from this file
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 }
 
 func AboutUs(c *gin.Context) {
@@ -24,7 +24,8 @@ func main() {
 	router.GET("/", WelcomeIndex)
 	router.GET("/about-us", AboutUs)
 	router.NoRoute(Custom404)
-	router.LoadHTMLGlob("templates/*.tmpl") // 2. load all the files in this directory
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.Static("/assets", "assets") //        <- files in assets folder will work
 	port := 3000
 	router.Run(fmt.Sprintf(":%d", port))
 }
