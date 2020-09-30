17d9702a6610f283411b586922f66b450eba56e0
Andrew Arrow <oneone@gmail.com>
2020-09-29 17:30:01 -0700

    real html

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func WelcomeIndex(c *gin.Context) {
-	c.String(http.StatusOK, "hello")
+	c.Data(http.StatusOK, "text/html;",
+		[]byte("<html><body><h1 style='font-size:72px;color:red;'>Hello</h1></body></html>")) // <-- real HTML
 }
 
 func AboutUs(c *gin.Context) {
@@ -23,7 +24,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", WelcomeIndex)
 	router.GET("/about-us", AboutUs)
-	router.NoRoute(Custom404) //         <------- notice the special NoRotue func
+	router.NoRoute(Custom404)
 	port := 3000
 	router.Run(fmt.Sprintf(":%d", port))
 }
